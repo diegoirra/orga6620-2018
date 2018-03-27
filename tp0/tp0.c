788b147bd10c01c36177241e1b758c2c2d0cd1f5
Matias <fonseca.196@fi.uba.ar>
2018-03-27 12:48:46 -0300

    agrego calcular el valor a cada pixel

--- tp0/tp0.c
+++ tp0/tp0.c
@@ -4,10 +4,11 @@
 #include <ctype.h>
 #include <complex.h>
 #include <errno.h>
+#include <math.h>
 
 #define SUCCESS 0
 #define ERROR_OUTPUT -1
-
+#define N 255
 #define RESOLUTION_DEFAULT "640x480"
 #define CENTER_DEFAUlT I
 #define WIDTH_DEFAULT 2
@@ -31,7 +32,7 @@ void handle(int res_horizontal,int res_vertical, double complex center,
 	printf("Resolution: %dx%d\n", res_horizontal, res_vertical);
 	printf("Centro de coordenadas del plano compejo: %f%+fi\n", creal(center), cimag(center));
 	printf("Ancho: %f. Altura: %f.\n", width, height);
-	printf("Seed: %f%+fi\n", creal(seed), cimag(seed));
+    printf("Seed: %f%+fi\n", creal(seed), cimag(seed));
 }
 
 
@@ -43,26 +44,28 @@ int main(int argc, char** argv) {
 	double height = HEIGHT_DEFAULT;
 	double complex seed = SEED_DEFAULT;
 	char* output_filename = OUT_FILE;
-
+    int salidaConsola = -1;
+    char* pathToExe = argv[0];
+    
 	int i;
     for (i = 1; i < argc; i++){
 
-    	if(strcmp(argv[i], "-r") == 0 && strcmp(argv[i], "--resolution") == 0){
+    	if(strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--resolution") == 0){
     		resolution = argv[i+1];
 		}
-		if(strcmp(argv[i], "-c") == 0 && strcmp(argv[i], "--center") == 0){
+		if(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--center") == 0){
 			center = string_to_complex(argv[i+1]);
 		}
-		if(strcmp(argv[i], "-w") == 0 && strcmp(argv[i], "--width") == 0){
+		if(strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--width") == 0){
 			width = atoi(argv[i+1]);
 		}
-		if(strcmp(argv[i], "-H") == 0 && strcmp(argv[i], "--height") == 0){
+		if(strcmp(argv[i], "-H") == 0 || strcmp(argv[i], "--height") == 0){
 			height = atoi(argv[i+1]);
 		}
-		if(strcmp(argv[i], "-s") == 0 && strcmp(argv[i], "--seed") == 0){
+		if(strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--seed") == 0){
 			seed = string_to_complex(argv[i+1]);
 		}
-		if(strcmp(argv[i], "-o") == 0 && strcmp(argv[i], "--output") == 0){
+		if(strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0){
 			output_filename = argv[i+1];
 		}
     }
@@ -73,9 +76,16 @@ int main(int argc, char** argv) {
     	printf("Error obteniendo resolucion. Usando default\n");
     	sscanf(RESOLUTION_DEFAULT, "%dx%d", &res_horizontal, &res_vertical);
     }
+    
+    
+    float pasoH = ((width) / (float)(res_horizontal))/2;
+    float pasoV = ((height) / (float)(res_vertical))/2;
+    int contadorBrillo;
+    float temp, valorAbsoluto;
+    double complex pixel;
 
     FILE* outf;
-    if (strcmp(output_filename, "-") == 0){
+    if (strcmp(output_filename, "-") != 0){
     	outf = fopen(output_filename, "w");
     	if (outf == NULL){
     		fprintf(stderr, "No se pudo abrir archivo de salida %s\n", output_filename);
@@ -85,9 +95,36 @@ int main(int argc, char** argv) {
     } else outf = stdout;
 
     handle(res_horizontal, res_vertical, center, width, height, seed, outf);
-
-    if (outf != NULL && outf != stdin)
-    	fclose(outf);
+    
+    double real,imag;
+    
+   
+    for (int y = 1; y <= res_vertical; y++){
+        for (int x = 1; x <= res_horizontal; x++){
+            real = pasoH * (2 * x -1) - width / 2 + creall(center);
+            imag = -pasoV * (2 * y -1) + height / 2 + cimagl(center);
+            pixel = real + imag*I;
+            for (contadorBrillo = 0; contadorBrillo <= N; contadorBrillo++){
+                valorAbsoluto = sqrtf((creall(pixel) * creall(pixel)) + (cimagl(pixel) * cimagl(pixel)));
+                if (valorAbsoluto > 2) break;
+                temp = ((creall(pixel))*(creall(pixel)))-((cimagl(pixel))*(cimagl(pixel))) + creall(seed);
+                imag = ((cimagl(pixel))*(creall(pixel)))+((creall(pixel))*(cimagl(pixel))) + cimagl(seed);
+                pixel = temp + imag*I;
+            }
+            if (salidaConsola == 1) fprintf(stdout, "%d ", contadorBrillo); else fprintf(outf, "%d ", contadorBrillo );
+        }
+        if (salidaConsola == 1) fprintf(stdout, "\n"); else fprintf(outf, "\n");
+    }
+    
+    if (salidaConsola != 1){
+        rewind(outf);
+        fclose(outf);
+        printf("Archivo guardado en %s/%s\n", pathToExe, output_filename);
+    }
+    
+    
+    //if (outf != NULL && outf != stdin)
+   // 	fclose(outf);
 
 	return SUCCESS;
 }
