6c25713ac74b98fa3d5d8c3e71fa058f55360970
diegoirra <dgirra@hotmail.com>
2018-03-27 16:03:09 -0300

    adding test in makefile

--- tp0/tp0.c
+++ tp0/tp0.c
@@ -100,8 +100,8 @@ int main(int argc, char** argv) {
     
     if (salidaConsola == 1) fprintf(stdout, "P2 \n%d %d \n255 \n", res_horizontal, res_vertical); else fprintf(outf, "P2 \n%d %d \n255 \n", res_horizontal, res_vertical);
    
-    for (int y = 0; y <= (double)res_vertical; y++){
-        for (int x = 1; x <= (double)res_horizontal; x++){
+    for (int y = 0; y <= res_vertical; y++){
+        for (int x = 1; x <= res_horizontal; x++){
             real = pasoH * (2 * x -1) - width / 2 + creall(center);
             imag = -pasoV * (2 * y -1) + height / 2 + cimagl(center);
             pixel = real + imag*I;
@@ -120,7 +120,7 @@ int main(int argc, char** argv) {
     if (salidaConsola != 1){
         rewind(outf);
         fclose(outf);
-        printf("Archivo guardado en %s/%s\n", pathToExe, output_filename);
+        printf("Archivo guardado en %s/%s\n\n", pathToExe, output_filename);
     }
     
     
